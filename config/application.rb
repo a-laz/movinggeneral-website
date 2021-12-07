66bfd7de0c05a394f088f4e49177618ce89888ac
alex <mortaleusmarketing@gmail.com>
2021-12-07 15:16:38 -0500

    application rb fix

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Movinggeneral
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -18,5 +20,8 @@ module Movinggeneral
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.configure do |env|
+      env.export_concurrent = false
+    end
   end
 end
